71ce25009c341842abc82f2c9c75aa88f2c32fe7
Pieter-Jan Lavaerts <>
2023-08-16 22:17:41 +0200

    Add lombok support for jdtls and improve keybindings

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,10 +1,13 @@
 vim.lsp.set_log_level('debug')
 project_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1])
 project_name = vim.fs.basename(project_dir)
+lombok_location = vim.fs.normalize('~/.config/nvim/lombok.jar')
+vim.print(lombok_location)
 vim.lsp.start({
 	name = 'jdtls',
 	cmd = { 
 		'jdtls',
+		'--jvm-arg=-javaagent:'..lombok_location,
 		'-data', project_dir..'/../workspace/'..project_name
 	},
 	root_dir = project_dir
--- init.lua
+++ init.lua
@@ -2,19 +2,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.server_capabilities.hoverProvider then
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+			vim.keymap.set({'n', 'v'}, 'K', vim.lsp.buf.hover, { buffer = args.buf })
 		end
-		if client.server_capabilities.actionProvider then
-			vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { buffer = args.buf })
+		if client.server_capabilities.codeActionProvider then
+			vim.keymap.set({'n', 'v'}, 'ga', vim.lsp.buf.code_action, { buffer = args.buf })
 		end
 		if client.server_capabilities.referencesProvider then
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = args.buf })
+			vim.keymap.set({'n', 'v'}, 'gr', vim.lsp.buf.references, { buffer = args.buf })
 		end
 		if client.server_capabilities.definitionProvider then
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+			vim.keymap.set({'n', 'v'}, 'gd', vim.lsp.buf.definition, { buffer = args.buf })
 		end
 		if client.server_capabilities.declarationProvider then
-			vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, { buffer = args.buf })
+			vim.keymap.set({'n', 'v'}, 'gD', vim.lsp.buf.declaration, { buffer = args.buf })
 		end
 	end
 })
