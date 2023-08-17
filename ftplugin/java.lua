6b33c10b27e29758d7e08cb8434775d9aead1632
Pieter-Jan Lavaerts <>
2023-08-17 14:19:43 +0200

    Add lombok support, tree-sitter files for java and noh keybinding

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,7 +2,6 @@ vim.lsp.set_log_level('debug')
 project_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1])
 project_name = vim.fs.basename(project_dir)
 lombok_location = vim.fs.normalize('~/.config/nvim/lombok.jar')
-vim.print(lombok_location)
 vim.lsp.start({
 	name = 'jdtls',
 	cmd = { 
--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.keymap.set({'n', 'v'}, 'gc', "<cmd>noh<CR>", {})
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
